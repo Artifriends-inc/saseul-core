bef51eff14b8c3bba19a35b60fb7c11ab94630a5
Jaeryong Im <hackerrs.im@gmail.com>
2019-08-05 13:57:27 +0900

    ApiILoader 코멘트 및 TODO 추가

--- sourcedata/SaseulDefault/Common/ApiLoader.php
+++ sourcedata/SaseulDefault/Common/ApiLoader.php
@@ -12,6 +12,9 @@ class ApiLoader
 {
     public function __construct()
     {
+        // API 호출 시 최초로 호출되는 생성자로서 요청에 내용 확인 및 처리, 현재 POST로 모두 처리
+
+        // TODO: POST, GET 요청 분리
         if (isset($_SERVER['CONTENT_TYPE']) && strpos($_SERVER['CONTENT_TYPE'], 'json') > -1) {
             $_POST = json_decode(file_get_contents('php://input'), true);
             $_REQUEST = empty($_POST) ? $_REQUEST : array_merge($_REQUEST, $_POST);
@@ -20,14 +23,17 @@ class ApiLoader
         Service::initApi();
     }
 
+    // TODO: 실제 기능과 메서드의 이름이 일치하지 않아 명확하지 않음, 메서드 이름 변경 필요
     public function main(): void
     {
         $req = new HttpRequest($_REQUEST, $_SERVER, $_GET, $_POST);
         $resp = $this->route($req);
 
+        // 현재 아래 코드는 동작하지 않는다.
         $this->response($resp);
     }
 
+    // TODO: route 기능 분리, 너무 많은 기능을 담당(api 호출 및 응답 생성 등..)
     private function route(HttpRequest $req): HttpResponse
     {
         $uri = $req->getUri();
@@ -54,6 +60,7 @@ class ApiLoader
         $api = new $apiClassName();
         $api->main($req);
 
+        // TODO: 실제 동작하지 않는 부분, 추가 작업 필요
         return new HttpResponse($api->getResult()['code'], $api->getResult(), $api->getHtml());
     }
 
@@ -62,7 +69,6 @@ class ApiLoader
         http_response_code($resp->getCode());
         if (!headers_sent()) {
             $header = $resp->getHeader();
-
             foreach ($header as $key => $value) {
                 header("${key}: ${value}");
             }
