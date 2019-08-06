7983009f7d6fe2bc234c89cf6dedea79ab214c86
Thomas SJ Kang <ujuc@ujuc.kr>
2019-08-06 13:28:51 +0900

    SC-16 Add static analytics tool

--- .travis/set_php.sh
+++ .travis/set_php.sh
@@ -3,6 +3,9 @@
 # set php.ini
 phpenv config-add $TRAVIS_BUILD_DIR/conf/php.ini
 
+# Install extension
+pecl install ast
+
 # ed25519 extension
 git clone https://github.com/encedo/php-ed25519-ext.git
 cd php-ed25519-ext
@@ -10,15 +13,4 @@ phpize
 ./configure
 make
 make install
-
-# Install extension
-pecl install xdebug-2.7.2 memcached mongodb ast
-
-echo '
-extension=xdebug.so
-extension=memcached.so
-extension=mongodb.so
-extension=ast.so
-extension=ed25519.so
-' >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-
+cp ./modules/ed25519.so  ~/.phpenv/versions/$(phpenv version-name)/lib/php/extensions/no-debug-zts-20180731/
