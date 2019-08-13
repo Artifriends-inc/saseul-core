5ed805adec3fb90ab25e15b83fe1de5911c36205
Jaeryong Im <hackerrs.im@gmail.com>
2019-08-13 15:44:26 +0900

    GetTransaction 리팩터링
    - AbstractRequest 추상클래스 추가
    - GetTransaction 클래스가 AbstractRequest를 구현하도록 변경
    
    리팩터링의 이유는 다음과 같다.
    
    Request API의 모든 getValidity 메서드는 똑같은 검증을 하고 있으며,
    initialize 메서드 또한 비슷한 작업을 하고 있다.
    이를 추상 클래스인 AbstractRequest로 추출하도록 한다.
    
    모든 Reqeust API들은 점차적으로 AbstractRequest를 상속하도록
    변경 예정이다.

---
+++ sourcedata/SaseulDefault/Common/AbstractRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Saseul\Common;
+
+use Saseul\System\Key;
+use Saseul\Version;
+
+abstract class AbstractRequest implements RequestInterface
+{
+    protected $thash;
+    protected $public_key;
+    protected $signature;
+    protected $type;
+    protected $version;
+    protected $from;
+    protected $timestamp;
+
+    public function initialize(array $request, string $thash, string $public_key, string $signature): void
+    {
+        $this->thash = $thash;
+        $this->public_key = $public_key;
+        $this->signature = $signature;
+
+        $this->type = $request['type'] ?? '';
+        $this->version = $request['version'] ?? '';
+        $this->from = $request['from'] ?? '';
+        $this->timestamp = $request['timestamp'] ?? 0;
+    }
+
+    public function getValidity(): bool
+    {
+        $calledRequest = new \ReflectionClass(get_class($this));
+
+        return Version::isValid($this->version)
+            && !empty($this->timestamp)
+            && $this->type === $calledRequest->getShortName()
+            && Key::isValidAddress($this->from, $this->public_key)
+            && Key::isValidSignature($this->thash, $this->public_key, $this->signature);
+    }
+
+    abstract public function getResponse(): array;
+}
--- sourcedata/SaseulDefault/Version.php
+++ sourcedata/SaseulDefault/Version.php
@@ -9,6 +9,7 @@ class Version
 
     public static function isValid($version)
     {
+         // TODO: 현재 버전에 문자가 섞여 있어도 유효한 값으로 인식, 의도한 바인지 검토
         return is_string($version) && !empty($version) && (mb_strlen($version) < self::LENGTH_LIMIT);
     }
 }
