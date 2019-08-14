d172eec307da59e416188705899fc9800639dad8
Jaeryong Im <hackerrs.im@gmail.com>
2019-08-14 17:34:27 +0900

    Refactoring GetMyTransaction
    GetMyTransaction 클래스가 AbstractRequest를 상속하도록 리팩터링한다.

--- sourcedata/SaseulDefault/Custom/Request/GetMyTransaction.php
+++ sourcedata/SaseulDefault/Custom/Request/GetMyTransaction.php
@@ -2,51 +2,13 @@
 
 namespace Saseul\Custom\Request;
 
-use Saseul\Common\RequestInterface;
+use Saseul\Common\AbstractRequest;
 use Saseul\Constant\MongoDbConfig;
 use Saseul\System\Database;
-use Saseul\System\Key;
 use Saseul\Util\Parser;
-use Saseul\Version;
 
-class GetMyTransaction implements RequestInterface
+class GetMyTransaction extends AbstractRequest
 {
-    public const TYPE = 'GetMyTransaction';
-
-    protected $request;
-    protected $thash;
-    protected $public_key;
-    protected $signature;
-
-    private $type;
-    private $version;
-    private $from;
-    private $transactional_data;
-    private $timestamp;
-
-    public function initialize(array $request, string $thash, string $public_key, string $signature): void
-    {
-        $this->request = $request;
-        $this->thash = $thash;
-        $this->public_key = $public_key;
-        $this->signature = $signature;
-
-        $this->type = $this->request['type'] ?? '';
-        $this->version = $this->request['version'] ?? '';
-        $this->from = $this->request['from'] ?? '';
-        $this->transactional_data = $this->request['transactional_data'] ?? '';
-        $this->timestamp = $this->request['timestamp'] ?? 0;
-    }
-
-    public function getValidity(): bool
-    {
-        return Version::isValid($this->version)
-            && !empty($this->timestamp)
-            && $this->type === self::TYPE
-            && Key::isValidAddress($this->from, $this->public_key)
-            && Key::isValidSignature($this->thash, $this->public_key, $this->signature);
-    }
-
     public function getResponse(): array
     {
         $db = Database::GetInstance();
