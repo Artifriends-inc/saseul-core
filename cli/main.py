a2ed3f2de765271e5edfc2367036d42644e372ac
Thomas SJ Kang <ujuc@ujuc.kr>
2019-08-14 15:08:12 +0900

    SC-45 Add command of create env file
    `.env` 파일을 생성하는 스크립트 작성.

--- cli/main.py
+++ cli/main.py
@@ -1,6 +1,6 @@
 from invoke import Collection, Program
 
-from cli import docker, node
+from cli import docker, node, env
 
 
 def run():
@@ -8,4 +8,5 @@ def run():
     program.namespace = Collection()
     program.namespace.add_collection(Collection.from_module(node))
     program.namespace.add_collection(Collection.from_module(docker))
+    program.namespace.add_collection(Collection.from_module(env))
     return program.run()
