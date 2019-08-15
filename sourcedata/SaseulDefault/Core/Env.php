e391a8d8d294ea7877ea98d253058dfe9e0dacac
Thomas SJ Kang <ujuc@ujuc.kr>
2019-08-15 17:29:10 +0900

    SC-47 Change load env values

--- sourcedata/SaseulDefault/Core/Env.php
+++ sourcedata/SaseulDefault/Core/Env.php
@@ -2,9 +2,6 @@
 
 namespace Saseul\Core;
 
-use Saseul\Constant\Structure;
-use Saseul\Util\TypeChecker;
-
 class Env
 {
     public static $memcached = [
@@ -26,11 +23,16 @@ class Env
     ];
 
     public static $genesis = [
-        'host' => '54.180.9.16',
-        'address' => '0x6f1b0f1ae759165a92d2e7d0b4cae328a1403aa5e35a85',
-        'coin_amount' => '1000000000000000',
-        'deposit_amount' => '200000000000000',
+        'host' => '',
+        'address' => '',
+        'coin_amount' => '',
+        'deposit_amount' => '',
         'key' => []
+    ];
+
+    public static $log = [
+        'path' => '',
+        'level' => ''
     ];
 
     public static function loadGenesisKey(string $path): void
@@ -41,23 +43,47 @@ class Env
         self::$genesis['key'] = $genesis_key;
     }
 
+    /**
+     * ENV 값들을 불러온다.
+     */
     public static function load(): void
     {
-        // TODO: env 파일 찾는 로직 필요.
-        if (!is_file(SASEUL_DIR . '/env.default')) {
-            return;
-        }
+        // Node info
+        self::$nodeInfo['host'] = self::getFromEnv('NODE_HOST');
+        self::$nodeInfo['address'] = self::getFromEnv('NODE_ADDRESS');
+        self::$nodeInfo['public_key'] = self::getFromEnv('NODE_PUBLIC_KEY');
+        self::$nodeInfo['private_key'] = self::getFromEnv('NODE_PRIVATE_KEY');
 
-        $env = file_get_contents(SASEUL_DIR . '/env.default');
-        $env = json_decode($env, true);
-
-        if (TypeChecker::StructureCheck(Structure::ENV, $env)) {
-            self::$memcached = $env['memcached'];
-            self::$mongoDb = $env['mongo_db'];
-            self::$nodeInfo = $env['node_info'];
-            self::$genesis = $env['genesis'];
-        }
-
+        // Genesis
+        self::$genesis['host'] = self::getFromEnv('GENESIS_HOST');
+        self::$genesis['address'] = self::getFromEnv('GENESIS_ADDRESS');
+        self::$genesis['coin_amount'] = self::getFromEnv('GENESIS_COIN_VALUE');
+        self::$genesis['deposit_amount'] = self::getFromEnv('GENESIS_DEPOSIT_VALUE');
         self::loadGenesisKey(SASEUL_DIR . '/data/genesis_key.json');
+
+        // Log
+        self::$log['path'] = self::getFromEnv('LOG_PATH');
+        self::$log['level'] = self::getFromEnv('LOG_LEVEL');
+    }
+
+    /**
+     * Check environment param.
+     *
+     * @param string $key
+     *
+     * @return false|string If $key is missing, return false.
+     */
+    private static function getFromEnv(string $key)
+    {
+        $value = getenv($key);
+
+        if (empty($value)) {
+            // Todo: Add logging message
+            echo "Environment variables failed assertions: {$key} is messing.\n";
+
+            return false;
+        }
+
+        return $value;
     }
 }
