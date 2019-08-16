f561964923985be4d720e28ec06e01a318c45b79
Jaeryong Im <hackerrs.im@gmail.com>
2019-08-16 10:28:28 +0900

    Refactoring GetAccountInfo
    GetAccountInfo클래스가 AbstractRequest를 상속하도록 변경한다.

--- sourcedata/SaseulDefault/Custom/Request/GetAccountInfo.php
+++ sourcedata/SaseulDefault/Custom/Request/GetAccountInfo.php
@@ -2,51 +2,13 @@
 
 namespace Saseul\Custom\Request;
 
-use Saseul\Common\RequestInterface;
+use Saseul\Common\AbstractRequest;
 use Saseul\Custom\Method\Attributes;
 use Saseul\Custom\Method\Coin;
 use Saseul\Custom\Method\Token;
-use Saseul\System\Key;
-use Saseul\Version;
 
-class GetAccountInfo implements RequestInterface
+class GetAccountInfo extends AbstractRequest
 {
-    public const TYPE = 'GetAccountInfo';
-
-    protected $request;
-    protected $thash;
-    protected $public_key;
-    protected $signature;
-
-    private $type;
-    private $version;
-    private $from;
-    private $transactional_data;
-    private $timestamp;
-
-    public function initialize(array $request, string $thash, string $public_key, string $signature): void
-    {
-        $this->request = $request;
-        $this->thash = $thash;
-        $this->public_key = $public_key;
-        $this->signature = $signature;
-
-        $this->type = $this->request['type'] ?? '';
-        $this->version = $this->request['version'] ?? '';
-        $this->from = $this->request['from'] ?? '';
-        $this->transactional_data = $this->request['transactional_data'] ?? '';
-        $this->timestamp = $this->request['timestamp'] ?? 0;
-    }
-
-    public function getValidity(): bool
-    {
-        return Version::isValid($this->version)
-            && !empty($this->timestamp)
-            && $this->type === self::TYPE
-            && Key::isValidAddress($this->from, $this->public_key)
-            && Key::isValidSignature($this->thash, $this->public_key, $this->signature);
-    }
-
     public function getResponse(): array
     {
         $from = $this->request['from'];
