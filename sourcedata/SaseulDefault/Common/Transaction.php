a55f755418ebfdbf7a70f7af79b9b5c6fa3b6038
Jaeryong Im <hackerrs.im@gmail.com>
2019-08-26 16:43:46 +0900

    Refactoring SendToken api
    SendToken Api가 AbstractTransaction을 상속받도록 변경한다.
    기존에 상속했던 Transaction 클래스의 메서드를 실행 시키면
    AbstractTransaction 추상 클래스의 메서드가 호출되도록 변경하며,
    이후 모든 Transaction Api가 AbstractTransaction을 상속하게되면
    직접 호출하도록 변경한다.

--- sourcedata/SaseulDefault/Common/Transaction.php
+++ sourcedata/SaseulDefault/Common/Transaction.php
@@ -4,6 +4,12 @@ namespace Saseul\Common;
 
 use Saseul\Constant\Decision;
 
+/**
+ * @deprecated
+ * Transaction 클래스를 상속하는 Api는 AbstractTransaction으로
+ * 대체될 예정이며, 전체 Transaction Api가 AbstractTransaction을 상속할 때
+ * 제거 되어야 한다.
+ */
 class Transaction
 {
     public function _Init($transaction, $thash, $public_key, $signature)
--- sourcedata/SaseulDefault/Custom/Transaction/SendToken.php
+++ sourcedata/SaseulDefault/Custom/Transaction/SendToken.php
@@ -2,95 +2,108 @@
 
 namespace Saseul\Custom\Transaction;
 
-use Saseul\Common\Transaction;
+use Saseul\Common\AbstractTransaction;
 use Saseul\Constant\Account;
 use Saseul\Constant\Decision;
 use Saseul\Custom\Status\Token;
-use Saseul\System\Key;
-use Saseul\Version;
 
-class SendToken extends Transaction
+class SendToken extends AbstractTransaction
 {
-    public const TYPE = 'SendToken';
-
-    protected $transaction;
-    protected $thash;
-    protected $public_key;
-    protected $signature;
-
-    protected $status_key;
-
-    private $type;
-    private $version;
-    private $from;
     private $to;
     private $token_name;
     private $amount;
-    private $transactional_data;
-    private $timestamp;
-
     private $from_token_balance;
     private $to_token_balance;
 
+    /**
+     * @deprecated
+     * 기존의 _Init 메서드의 기능은 initialize 메서드가 담당하며
+     * 다른 모든 Transaction Api 가 AbstractTransaction 을 구현을 완료하면
+     * initialize 로 대체 되고 제거되어야 한다.
+     *
+     * @param mixed $transaction
+     * @param mixed $thash
+     * @param mixed $public_key
+     * @param mixed $signature
+     */
     public function _Init($transaction, $thash, $public_key, $signature)
     {
-        $this->transaction = $transaction;
-        $this->thash = $thash;
-        $this->public_key = $public_key;
-        $this->signature = $signature;
-
-        if (isset($this->transaction['type'])) {
-            $this->type = $this->transaction['type'];
-        }
-        if (isset($this->transaction['version'])) {
-            $this->version = $this->transaction['version'];
-        }
-        if (isset($this->transaction['from'])) {
-            $this->from = $this->transaction['from'];
-        }
-        if (isset($this->transaction['to'])) {
-            $this->to = $this->transaction['to'];
-        }
-        if (isset($this->transaction['token_name'])) {
-            $this->token_name = $this->transaction['token_name'];
-        }
-        if (isset($this->transaction['amount'])) {
-            $this->amount = $this->transaction['amount'];
-        }
-        if (isset($this->transaction['transactional_data'])) {
-            $this->transactional_data = $this->transaction['transactional_data'];
-        }
-        if (isset($this->transaction['timestamp'])) {
-            $this->timestamp = $this->transaction['timestamp'];
-        }
+        $this->initialize($transaction, $thash, $public_key, $signature);
     }
 
+    public function initialize($transaction, $thash, $public_key, $signature): void
+    {
+        parent::initialize($transaction, $thash, $public_key, $signature);
+
+        $this->to = $transaction['to'] ?? null;
+        $this->token_name = $transaction['token_name'] ?? null;
+        $this->amount = $transaction['amount'] ?? null;
+    }
+
+    /**
+     * @deprecated
+     * 기존의 _GetValidity 메서드의 기능은 getValidity 메서드가 담당하며
+     * 다른 모든 Transaction Api 가 AbstractTransaction 을 구현을 완료하면
+     * getValidity 로 대체 되고 제거되어야 한다.
+     */
     public function _GetValidity(): bool
     {
-        return Version::isValid($this->version)
-            && is_string($this->to)
-            && is_numeric($this->amount)
-            && is_numeric($this->timestamp)
-            && $this->type === self::TYPE
-            && (mb_strlen($this->to) === Account::ADDRESS_SIZE)
-            && ((int) $this->amount > 0)
-            && Key::isValidAddress($this->from, $this->public_key)
-            && Key::isValidSignature($this->thash, $this->public_key, $this->signature);
+        return $this->getValidity();
     }
 
+    public function getValidity(): bool
+    {
+        return parent::getValidity()
+            && $this->isValidTo()
+            && $this->isValidAmount();
+    }
+
+    /**
+     * @deprecated
+     * 기존의 _LoadStatus 메서드의 기능은 loadStatus 메서드가 담당하며
+     * 다른 모든 Transaction Api 가 AbstractTransaction 을 구현을 완료하면
+     * loadStatus 로 대체 되고 제거되어야 한다.
+     */
     public function _LoadStatus()
+    {
+        $this->loadStatus();
+    }
+
+    public function loadStatus()
     {
         Token::LoadToken($this->from, $this->token_name);
         Token::LoadToken($this->to, $this->token_name);
     }
 
+    /**
+     * @deprecated
+     * 기존의 _GetStatus 메서드의 기능은 getStatus 메서드가 담당하며
+     * 다른 모든 Transaction Api 가 AbstractTransaction 을 구현을 완료하면
+     * getStatus 로 대체 되고 제거되어야 한다.
+     */
     public function _GetStatus()
+    {
+        $this->getStatus();
+    }
+
+    public function getStatus()
     {
         $this->from_token_balance = Token::GetBalance($this->from, $this->token_name);
         $this->to_token_balance = Token::GetBalance($this->to, $this->token_name);
     }
 
+    /**
+     * @deprecated
+     * 기존의 _MakeDecision 메서드의 기능은 makeDecision 메서드가 담당하며
+     * 다른 모든 Transaction Api 가 AbstractTransaction 을 구현을 완료하면
+     * makeDecision 로 대체 되고 제거되어야 한다.
+     */
     public function _MakeDecision()
+    {
+        $this->makeDecision();
+    }
+
+    public function makeDecision()
     {
         if ((int) $this->amount > (int) $this->from_token_balance) {
             return Decision::REJECT;
@@ -99,12 +112,35 @@ class SendToken extends Transaction
         return Decision::ACCEPT;
     }
 
+    /**
+     * @deprecated
+     * 기존의 _SetStatus 메서드의 기능은 setStatus 메서드가 담당하며
+     * 다른 모든 Transaction Api 가 AbstractTransaction 을 구현을 완료하면
+     * setStatus 로 대체 되고 제거되어야 한다.
+     */
     public function _SetStatus()
+    {
+        $this->setStatus();
+    }
+
+    public function setStatus()
     {
         $this->from_token_balance = (int) $this->from_token_balance - (int) $this->amount;
         $this->to_token_balance = (int) $this->to_token_balance + (int) $this->amount;
 
         Token::SetBalance($this->from, $this->token_name, $this->from_token_balance);
         Token::SetBalance($this->to, $this->token_name, $this->to_token_balance);
+    }
+
+    private function isValidTo(): bool
+    {
+        return is_string($this->to)
+            && (mb_strlen($this->to) === Account::ADDRESS_SIZE);
+    }
+
+    private function isValidAmount(): bool
+    {
+        return is_numeric($this->amount)
+            && ((int) $this->amount > 0);
     }
 }
