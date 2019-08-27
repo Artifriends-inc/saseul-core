ba05e0c4f5f5e46e1ec457a22b0bcbdccade08a6
Jaeryong Im <hackerrs.im@gmail.com>
2019-08-27 14:48:51 +0900

    Refactoring Withdraw api
    Withdraw Api가 AbstractTransaction을 상속받도록 변경한다.

--- sourcedata/SaseulDefault/Custom/Transaction/Withdraw.php
+++ sourcedata/SaseulDefault/Custom/Transaction/Withdraw.php
@@ -2,88 +2,66 @@
 
 namespace Saseul\Custom\Transaction;
 
-use Saseul\Common\Transaction;
+use Saseul\Common\AbstractTransaction;
 use Saseul\Constant\Decision;
 use Saseul\Core\Env;
 use Saseul\Custom\Status\Coin;
 use Saseul\Custom\Status\Fee;
-use Saseul\System\Key;
-use Saseul\Version;
 
-class Withdraw extends Transaction
+class Withdraw extends AbstractTransaction
 {
-    public const TYPE = 'Withdraw';
-
-    protected $transaction;
-    protected $thash;
-    protected $public_key;
-    protected $signature;
-
-    protected $status_key;
-
-    private $type;
-    private $version;
-    private $from;
-    private $amount;
+    private $withdrawal_amount;
     private $fee;
-    private $transactional_data;
-    private $timestamp;
-
     private $from_balance;
     private $from_deposit;
     private $coin_fee;
 
     public function _Init($transaction, $thash, $public_key, $signature)
     {
-        $this->transaction = $transaction;
-        $this->thash = $thash;
-        $this->public_key = $public_key;
-        $this->signature = $signature;
+        $this->initialize($transaction, $thash, $public_key, $signature);
+    }
 
-        if (isset($this->transaction['type'])) {
-            $this->type = $this->transaction['type'];
-        }
-        if (isset($this->transaction['version'])) {
-            $this->version = $this->transaction['version'];
-        }
-        if (isset($this->transaction['from'])) {
-            $this->from = $this->transaction['from'];
-        }
-        if (isset($this->transaction['amount'])) {
-            $this->amount = $this->transaction['amount'];
-        }
-        if (isset($this->transaction['fee'])) {
-            $this->fee = $this->transaction['fee'];
-        }
-        if (isset($this->transaction['transactional_data'])) {
-            $this->transactional_data = $this->transaction['transactional_data'];
-        }
-        if (isset($this->transaction['timestamp'])) {
-            $this->timestamp = $this->transaction['timestamp'];
-        }
+    public function initialize(
+        $transaction,
+        $thash,
+        $public_key,
+        $signature
+    ): void {
+        parent::initialize($transaction, $thash, $public_key, $signature);
+
+        $this->withdrawal_amount = $transaction['amount'] ?? null;
+        $this->fee = $transaction['fee'] ?? null;
     }
 
     public function _GetValidity(): bool
     {
-        return Version::isValid($this->version)
-            && is_numeric($this->amount)
-            && is_numeric($this->fee)
-            && is_numeric($this->timestamp)
-            && $this->type === self::TYPE
-            && ((int) $this->amount <= Env::$genesis['coin_amount'])
-            && ((int) $this->amount > 0)
-            && ((int) $this->fee >= 0)
-            && Key::isValidAddress($this->from, $this->public_key)
-            && Key::isValidSignature($this->thash, $this->public_key, $this->signature);
+        return $this->getValidity();
+    }
+
+    public function getValidity(): bool
+    {
+        return parent::getValidity()
+            && $this->isValidWithdrawalAmount()
+            && $this->isValidFee();
     }
 
     public function _LoadStatus()
+    {
+        $this->loadStatus();
+    }
+
+    public function loadStatus()
     {
         Coin::LoadBalance($this->from);
         Coin::LoadDeposit($this->from);
     }
 
     public function _GetStatus()
+    {
+        $this->getStatus();
+    }
+
+    public function getStatus()
     {
         $this->from_balance = Coin::GetBalance($this->from);
         $this->from_deposit = Coin::GetDeposit($this->from);
@@ -92,7 +70,12 @@ class Withdraw extends Transaction
 
     public function _MakeDecision()
     {
-        if ((int) $this->amount + (int) $this->fee > (int) $this->from_deposit) {
+        $this->makeDecision();
+    }
+
+    public function makeDecision()
+    {
+        if ((int) $this->withdrawal_amount + (int) $this->fee > (int) $this->from_deposit) {
             return Decision::REJECT;
         }
 
@@ -101,13 +84,31 @@ class Withdraw extends Transaction
 
     public function _SetStatus()
     {
-        $this->from_deposit = (int) $this->from_deposit - (int) $this->amount;
+        $this->setStatus();
+    }
+
+    public function setStatus()
+    {
+        $this->from_deposit = (int) $this->from_deposit - (int) $this->withdrawal_amount;
         $this->from_deposit = (int) $this->from_deposit - (int) $this->fee;
-        $this->from_balance = (int) $this->from_balance + (int) $this->amount;
+        $this->from_balance = (int) $this->from_balance + (int) $this->withdrawal_amount;
         $this->coin_fee = (int) $this->coin_fee + (int) $this->fee;
 
         Coin::SetBalance($this->from, $this->from_balance);
         Coin::SetDeposit($this->from, $this->from_deposit);
         Fee::SetFee($this->coin_fee);
+    }
+
+    private function isValidWithdrawalAmount(): bool
+    {
+        return is_numeric($this->withdrawal_amount)
+            && ((int) $this->withdrawal_amount > 0)
+            && ((int) $this->withdrawal_amount <= Env::$genesis['coin_amount']);
+    }
+
+    private function isValidFee(): bool
+    {
+        return is_numeric($this->fee)
+            && ((int) $this->fee >= 0);
     }
 }
