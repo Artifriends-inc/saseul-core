bda05a65050677e70e4f805c569649ad1ac16c42
Jaeryong Im <hackerrs.im@gmail.com>
2019-10-02 11:03:15 +0900

    SC-163 remove deprecated methods
    SendToken Api에서 deprecated로 지정된 메서드들을 제거한다.
    또한 연관된 테스트들도 제거한다.

--- sourcedata/SaseulDefault/Custom/Transaction/SendToken.php
+++ sourcedata/SaseulDefault/Custom/Transaction/SendToken.php
@@ -15,22 +15,6 @@ class SendToken extends AbstractTransaction
     private $from_token_balance;
     private $to_token_balance;
 
-    /**
-     * @deprecated
-     * 기존의 _Init 메서드의 기능은 initialize 메서드가 담당하며
-     * 다른 모든 Transaction Api 가 AbstractTransaction 을 구현을 완료하면
-     * initialize 로 대체 되고 제거되어야 한다.
-     *
-     * @param mixed $transaction
-     * @param mixed $thash
-     * @param mixed $public_key
-     * @param mixed $signature
-     */
-    public function _Init($transaction, $thash, $public_key, $signature)
-    {
-        $this->initialize($transaction, $thash, $public_key, $signature);
-    }
-
     public function initialize($transaction, $thash, $public_key, $signature): void
     {
         parent::initialize($transaction, $thash, $public_key, $signature);
@@ -40,33 +24,11 @@ class SendToken extends AbstractTransaction
         $this->amount = $transaction['amount'] ?? null;
     }
 
-    /**
-     * @deprecated
-     * 기존의 _GetValidity 메서드의 기능은 getValidity 메서드가 담당하며
-     * 다른 모든 Transaction Api 가 AbstractTransaction 을 구현을 완료하면
-     * getValidity 로 대체 되고 제거되어야 한다.
-     */
-    public function _GetValidity(): bool
-    {
-        return $this->getValidity();
-    }
-
     public function getValidity(): bool
     {
         return parent::getValidity()
             && $this->isValidTo()
             && $this->isValidAmount();
-    }
-
-    /**
-     * @deprecated
-     * 기존의 _LoadStatus 메서드의 기능은 loadStatus 메서드가 담당하며
-     * 다른 모든 Transaction Api 가 AbstractTransaction 을 구현을 완료하면
-     * loadStatus 로 대체 되고 제거되어야 한다.
-     */
-    public function _LoadStatus()
-    {
-        $this->loadStatus();
     }
 
     public function loadStatus()
@@ -75,32 +37,10 @@ class SendToken extends AbstractTransaction
         Token::LoadToken($this->to, $this->token_name);
     }
 
-    /**
-     * @deprecated
-     * 기존의 _GetStatus 메서드의 기능은 getStatus 메서드가 담당하며
-     * 다른 모든 Transaction Api 가 AbstractTransaction 을 구현을 완료하면
-     * getStatus 로 대체 되고 제거되어야 한다.
-     */
-    public function _GetStatus()
-    {
-        $this->getStatus();
-    }
-
     public function getStatus()
     {
         $this->from_token_balance = Token::GetBalance($this->from, $this->token_name);
         $this->to_token_balance = Token::GetBalance($this->to, $this->token_name);
-    }
-
-    /**
-     * @deprecated
-     * 기존의 _MakeDecision 메서드의 기능은 makeDecision 메서드가 담당하며
-     * 다른 모든 Transaction Api 가 AbstractTransaction 을 구현을 완료하면
-     * makeDecision 로 대체 되고 제거되어야 한다.
-     */
-    public function _MakeDecision()
-    {
-        return $this->makeDecision();
     }
 
     public function makeDecision()
@@ -110,17 +50,6 @@ class SendToken extends AbstractTransaction
         }
 
         return Decision::ACCEPT;
-    }
-
-    /**
-     * @deprecated
-     * 기존의 _SetStatus 메서드의 기능은 setStatus 메서드가 담당하며
-     * 다른 모든 Transaction Api 가 AbstractTransaction 을 구현을 완료하면
-     * setStatus 로 대체 되고 제거되어야 한다.
-     */
-    public function _SetStatus()
-    {
-        $this->setStatus();
     }
 
     public function setStatus()
