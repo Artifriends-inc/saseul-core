ce5f610f1ce358dcf0b603995c6b321e00ffcca7
Thomas SJ Kang <ujuc@ujuc.kr>
2019-10-17 23:47:46 +0900

    SC-189 Attributes::isFullNode 삭제

--- sourcedata/SaseulDefault/Custom/Method/Attributes.php
+++ sourcedata/SaseulDefault/Custom/Method/Attributes.php
@@ -5,7 +5,6 @@ namespace Saseul\Custom\Method;
 use Saseul\Constant\MongoDb;
 use Saseul\Constant\Role;
 use Saseul\System\Database;
-use Saseul\Util\Mongo;
 
 /**
  * Class Attributes.
@@ -46,31 +45,6 @@ class Attributes
         return $nodes;
     }
 
-    public static function IsFullNode($address, $query = ['key' => 'role', 'value' => ['$in' => Role::FULL_NODES]])
-    {
-        $db = Database::getInstance();
-        $query = array_merge(['address' => $address], $query);
-        $command = [
-            'count' => Mongo::COLLECTION_ATTRIBUTES,
-            'query' => $query,
-        ];
-
-        $rs = $db->Command(Mongo::DB_COMMITTED, $command);
-        $count = 0;
-
-        foreach ($rs as $item) {
-            $count = $item->n;
-
-            break;
-        }
-
-        if ($count > 0) {
-            return true;
-        }
-
-        return false;
-    }
-
     public static function GetValidator()
     {
         return self::GetFullNode(['key' => 'role', 'value' => Role::VALIDATOR]);
@@ -84,20 +58,5 @@ class Attributes
     public static function GetArbiter()
     {
         return self::GetFullNode(['key' => 'role', 'value' => Role::ARBITER]);
-    }
-
-    public static function IsValidator($address)
-    {
-        return self::IsFullNode($address, ['key' => 'role', 'value' => Role::VALIDATOR]);
-    }
-
-    public static function IsSupervisor($address)
-    {
-        return self::IsFullNode($address, ['key' => 'role', 'value' => Role::SUPERVISOR]);
-    }
-
-    public static function IsArbiter($address)
-    {
-        return self::IsFullNode($address, ['key' => 'role', 'value' => Role::ARBITER]);
     }
 }
