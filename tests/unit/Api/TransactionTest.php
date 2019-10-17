d0663f8759aeae0931f7ac4d7cf46d9d98773887
jaeryol <hackerrs.im@gmail.com>
2019-10-17 15:05:55 +0900

    SC-184 Add transaction api unit test
    Transaction Api 버그에 대한 테스트 케이스를 추가한다.
    버그에 대한 내용은 SC-182 을 참고한다.

--- tests/unit/Api/TransactionTest.php
+++ tests/unit/Api/TransactionTest.php
@@ -6,15 +6,22 @@ use PHPUnit\Framework\TestCase;
 use Saseul\Api\Transaction;
 use Saseul\Common\ExternalApi;
 use Saseul\Constant\Account;
+use Saseul\Constant\Directory;
+use Saseul\Core\Chunk;
 use Saseul\Core\Env;
+use Saseul\Core\NodeInfo;
+use Saseul\Core\Tracker;
 use Saseul\System\HttpRequest;
 use Saseul\System\HttpResponse;
 use Saseul\System\HttpStatus;
 use Saseul\System\Key;
 use Saseul\Util\DateTime;
+use Saseul\Util\Logger;
 
 class TransactionTest extends TestCase
 {
+    private $transaction;
+
     public function testSutInheritsExternalApi(): void
     {
         // Arrange
@@ -24,7 +31,7 @@ class TransactionTest extends TestCase
         $this->assertInstanceOf(ExternalApi::class, $sut);
     }
 
-    public function testValidTransactionReturnsOK(): void
+    public function testValidSendCoinTransactionReturnsOK(): void
     {
         // Arrange
         $this->prepareTransaction('SendCoin');
@@ -41,8 +48,28 @@ class TransactionTest extends TestCase
         $this->assertIsArray($actual->getData());
         $this->assertTrue(array_key_exists('message', $actual->getData()));
         $this->assertTrue(array_key_exists('transaction', $actual->getData()));
+        $this->assertAddedTransactionExactly();
         $this->assertTrue(array_key_exists('public_key', $actual->getData()));
         $this->assertTrue(array_key_exists('signature', $actual->getData()));
+    }
+
+    private function assertAddedTransactionExactly(): void
+    {
+        $actual = $this->getChunkOfTransaction();
+        $this->assertIsArray($actual);
+        $this->assertCount(count($actual), $this->transaction);
+        foreach ($actual as $key => $value)
+        {
+            $this->assertSame($this->transaction[$key], $value);
+        }
+    }
+
+    private function getChunkOfTransaction(): array
+    {
+        $apiChunkId = Chunk::getId($this->transaction['timestamp']);
+        $apiChunk = Chunk::getChunk(
+            Directory::API_CHUNKS . '/' . $apiChunkId . '.json');
+        return $apiChunk[0]['transaction'];
     }
 
     public function testGivenNonExistentTypeThenReturnsNotFound(): void
@@ -103,23 +130,24 @@ class TransactionTest extends TestCase
     {
         $amount = 1000;
         Env::$genesis['coin_amount'] = $amount + 100;
-        $transaction = [
+        $this->transaction = [
             'type' => $type,
             'from' => '0x6f258c97ad7848aef661465018dc48e55131eff91c4e20',
             'version' => '1.0',
-            'timestamp' => DateTime::Date(),
+            'timestamp' => DateTime::Microtime() + DateTime::Microtime(),
             'to' => str_repeat('*', Account::ADDRESS_SIZE),
             'amount' => $amount,
             'fee' => 100
         ];
 
-        $thash = hash('sha256', json_encode($transaction));
+        $thash = hash('sha256', json_encode($this->transaction, JSON_THROW_ON_ERROR));
         $private_key = 'a609aca90f9338da02e640c7df8ae760211bef48031973ee00000169dca49c4d';
         $public_key = '2704e240b3e806f476211694c2bda537bcf56941199f4447468021f9c3833a33';
         $signature = Key::makeSignature($thash, $private_key, $public_key);
 
         $_REQUEST = [
-            'transaction' => json_encode($transaction),
+            'transaction' => json_encode($this->transaction,
+                JSON_THROW_ON_ERROR),
             'public_key' => $public_key,
             'signature' => $signature
         ];
@@ -127,6 +155,7 @@ class TransactionTest extends TestCase
         $_SERVER['REQUEST_URI'] = '/transaction';
 
         $this->prepareNodeInfo();
+        Tracker::SetValidator(NodeInfo::getAddress());
     }
 
     private function prepareNodeInfo(): void
