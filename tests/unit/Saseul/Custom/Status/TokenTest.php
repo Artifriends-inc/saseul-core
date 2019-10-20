318431734509ba9eebf3b4db4c0241087212f164
Thomas SJ Kang <ujuc@ujuc.kr>
2019-10-20 15:57:12 +0900

    SC-193 Token Save 함수 DB 사용법 수정

---
+++ tests/unit/Saseul/Custom/Status/TokenTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Saseul\Tests\Unit\Custom\Status;
+
+use PHPUnit\Framework\TestCase;
+use Saseul\Custom\Status\Token;
+use Saseul\System\Database;
+
+class TokenTest extends TestCase
+{
+    private $db;
+
+    protected function setUp(): void
+    {
+        $this->db = Database::getInstance();
+        $this->db->getTokenCollection()->drop();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->db->getTokenCollection()->drop();
+    }
+
+    public function testGivenDataThenSave(): void
+    {
+        // Arrange
+        $address = '0x60aaaa';
+        $tokenName = 'Test10';
+
+        $this->db->getTokenCollection()->insertOne([
+            'address' => $address,
+            'token_name' => $tokenName,
+            'balance' => 1000,
+        ]);
+
+        Token::SetBalance($address, $tokenName, 1010);
+
+        // Act
+        Token::_Save();
+
+        // Assert
+        $actual = $this->db->getTokenCollection()->findOne(['address' => $address, 'token_name' => $tokenName]);
+        $this->assertSame(1010, $actual['balance']);
+    }
+}
