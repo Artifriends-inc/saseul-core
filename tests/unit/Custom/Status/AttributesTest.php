c3be6dc7e9130d34854c30f1ba619bc77f030379
Thomas SJ Kang <ujuc@ujuc.kr>
2019-10-22 11:34:28 +0900

    SC-197 Attributes _Save 함수 수정 (#119)

---
+++ tests/unit/Custom/Status/AttributesTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Saseul\Tests\Unit\Custom\Status;
+
+use PHPUnit\Framework\TestCase;
+use Saseul\Constant\Role;
+use Saseul\Custom\Status\Attributes;
+use Saseul\System\Database;
+
+class AttributesTest extends TestCase
+{
+    private $db;
+
+    protected function setUp(): void
+    {
+        $this->db = Database::getInstance();
+
+        $this->db->getAttributesCollection()->drop();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->db->getAttributesCollection()->drop();
+    }
+
+    public function testGivenDataThenSave(): void
+    {
+        // Arrange
+        $address = '0x6aaaa';
+
+        $this->db->getAttributesCollection()->insertOne([
+            'address' => '',
+            'key' => 'role',
+            'value' => Role::LIGHT,
+        ]);
+
+        Attributes::setRole($address, Role::VALIDATOR);
+
+        // Act
+        Attributes::_Save();
+
+        // Assert
+        $actual = $this->db->getAttributesCollection()->findOne(['address' => $address]);
+        $this->assertSame(Role::VALIDATOR, $actual['value']);
+    }
+}
