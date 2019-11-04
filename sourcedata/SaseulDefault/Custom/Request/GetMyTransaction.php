491d4c8f8c5df32e2d1a87c8c6d1d9a3861d3353
Thomas SJ Kang <ujuc@ujuc.kr>
2019-11-04 11:02:40 +0900

    SC-241 Edit GetMyTransaction (#136)
    * limit, offset 을 추가하여 pagination이 가능하도록 수정

--- sourcedata/SaseulDefault/Custom/Request/GetMyTransaction.php
+++ sourcedata/SaseulDefault/Custom/Request/GetMyTransaction.php
@@ -2,45 +2,62 @@
 
 namespace Saseul\Custom\Request;
 
-use MongoDB\Driver\Exception\Exception;
-use Saseul\Constant\MongoDb;
+use Exception;
 use Saseul\System\Database;
+use Saseul\Util\Logger;
 use Saseul\Util\Parser;
 
 /**
  * Class GetMyTransaction.
  * 요청한 Account 에 대한 Transaction 정보를 가져온다.
+ * 정렬: 시간 역순.
  */
 class GetMyTransaction extends AbstractRequest
 {
+    /** @var string */
+    private $limit;
+
+    /** @var string */
+    private $offset;
+
+    private $logger;
+
+    public function __construct()
+    {
+        $this->logger = Logger::getLogger(Logger::API);
+    }
+
+    public function initialize(array $request, string $thash, string $public_key, string $signature): void
+    {
+        parent::initialize($request, $thash, $public_key, $signature);
+
+        $this->limit = (int) (($request['limit'] !== '0') ? $request['limit'] : '10');
+        $this->offset = (int) $request['offset'];
+    }
+
     /**
      * @throws Exception
      *
-     * @return array (See below)
+     * @return array
      */
     public function getResponse(): array
     {
         $db = Database::getInstance();
 
-        $namespace = MongoDb::NAMESPACE_TRANSACTION;
-        $filter = ['public_key' => $this->public_key];
-        $opt = ['sort' => ['timestamp' => -1]];
-        $rs = $db->Query($namespace, $filter, $opt);
+        $cursor = $db->getTransactionsCollection()->find(
+            ['public_key' => $this->public_key],
+            [
+                'sort' => ['timestamp' => -1],
+                'limit' => $this->limit,
+                'skip' => ($this->limit * $this->offset),
+            ]
+        );
 
-        $max = 10;
-        $count = 0;
         $transactions = [];
-
-        foreach ($rs as $item) {
+        foreach ($cursor as $item) {
             $item = Parser::objectToArray($item);
             unset($item['_id']);
-
             $transactions[] = $item;
-            ++$count;
-
-            if ($count >= $max) {
-                break;
-            }
         }
 
         return $transactions;
