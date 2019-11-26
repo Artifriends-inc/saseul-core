d05d95fce13c53ac3f456272bddc3764652426f1
Thomas SJ Kang <ujuc@ujuc.kr>
2019-11-26 11:25:53 +0900

    SC-210 Status::Token _load 메소드 리펙토링을 한다
    * Token _load 메소드를 리펙토링 했다.
    * Test 추가하였다.
    
    [기타 작업]
    * 코드 컨벤션 및 주석 추가
    * 테스트를 위해 가지고 있는 데이터를 가져오는 함수 추가

--- sourcedata/SaseulDefault/Custom/Status/Token.php
+++ sourcedata/SaseulDefault/Custom/Status/Token.php
@@ -4,7 +4,6 @@ namespace Saseul\Custom\Status;
 
 use Exception;
 use Saseul\Common\Status;
-use Saseul\Constant\MongoDb;
 use Saseul\System\Database;
 
 class Token implements Status
@@ -13,24 +12,71 @@ class Token implements Status
     protected static $token_names = [];
     protected static $balances = [];
 
-    public static function LoadToken($address, $token_name)
+    /**
+     * 읽어올 Token 들을 정의한다.
+     *
+     * @param string $address    Account Address
+     * @param string $token_name Token 이름
+     */
+    public static function loadToken(string $address, string $token_name)
     {
         self::$addresses[] = $address;
         self::$token_names[] = $token_name;
     }
 
-    public static function GetBalance($address, $token_name)
+    /**
+     * Balance 정보를 가져온다.
+     *
+     * @param string $address    Account Address
+     * @param string $token_name Token 이름
+     *
+     * @return int 데이터가 없으면 0으로 반환된다.
+     */
+    public static function getBalance(string $address, string $token_name): int
     {
-        if (isset(self::$balances[$address][$token_name])) {
-            return self::$balances[$address][$token_name];
-        }
-
-        return 0;
+        return self::$balances[$address][$token_name] ?? 0;
     }
 
-    public static function SetBalance($address, $token_name, int $value)
+    /**
+     * Token balance 를 저장한다.
+     *
+     * @param string $address    Token 소유자 주소
+     * @param string $token_name Token 이름
+     * @param int    $value      Token 수량
+     */
+    public static function setBalance(string $address, string $token_name, int $value): void
     {
-        self::$balances[$address][$token_name] = (int) $value;
+        self::$balances[$address][$token_name] = $value;
+    }
+
+    /**
+     * 읽어올 주소를 반환한다.
+     *
+     * @return array
+     */
+    public function getAllAddressList(): array
+    {
+        return self::$addresses;
+    }
+
+    /**
+     * 읽어올 Token 이름을 반환한다.
+     *
+     * @return array
+     */
+    public function getAllTokenNameList(): array
+    {
+        return self::$token_names;
+    }
+
+    /**
+     * 쓰거나 읽어온 Token에 대한 balance를 반환한다.
+     *
+     * @return array
+     */
+    public function getAllTokenBalance(): array
+    {
+        return self::$balances;
     }
 
     /**
@@ -45,26 +91,25 @@ class Token implements Status
 
     /**
      * 저장되어 있는 Status 값을 읽어온다.
-     *
-     * @throws \MongoDB\Driver\Exception\Exception
      */
     public static function _load(): void
     {
         self::$addresses = array_values(array_unique(self::$addresses));
         self::$token_names = array_values(array_unique(self::$token_names));
 
-        if (count(self::$addresses) === 0) {
+        if (empty(self::$addresses)) {
             return;
         }
 
         $db = Database::getInstance();
-        $filter = ['address' => ['$in' => self::$addresses], 'token_name' => ['$in' => self::$token_names]];
-        $rs = $db->Query(MongoDb::NAMESPACE_TOKEN, $filter);
+        $filter = [
+            'address' => ['$in' => self::$addresses],
+            'token_name' => ['$in' => self::$token_names]
+        ];
+        $cursor = $db->getTokenCollection()->find($filter);
 
-        foreach ($rs as $item) {
-            if (isset($item->balance)) {
-                self::$balances[$item->address][$item->token_name] = $item->balance;
-            }
+        foreach ($cursor as $item) {
+            self::$balances[$item->address][$item->token_name] = $item->balance ?? 0;
         }
     }
 
--- tests/unit/Custom/Status/TokenTest.php
+++ tests/unit/Custom/Status/TokenTest.php
@@ -3,22 +3,31 @@
 namespace Saseul\Test\Unit\Custom\Status;
 
 use PHPUnit\Framework\TestCase;
+use Saseul\Core\NodeInfo;
 use Saseul\Custom\Status\Token;
 use Saseul\System\Database;
 
 class TokenTest extends TestCase
 {
-    private $db;
+    protected static $db;
+    protected static $sut;
 
-    protected function setUp(): void
+    public static function setUpBeforeClass(): void
     {
-        $this->db = Database::getInstance();
-        $this->db->getTokenCollection()->drop();
+        self::$db = Database::getInstance();
+        self::$db->getTokenCollection()->drop();
+
+        self::$sut = new Token();
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        self::$db->getTokenCollection()->drop();
     }
 
     protected function tearDown(): void
     {
-        $this->db->getTokenCollection()->drop();
+        self::$db->getTokenCollection()->drop();
     }
 
     public function testGivenDataThenSave(): void
@@ -27,19 +36,102 @@ class TokenTest extends TestCase
         $address = '0x60aaaa';
         $tokenName = 'Test10';
 
-        $this->db->getTokenCollection()->insertOne([
+        self::$db->getTokenCollection()->insertOne([
             'address' => $address,
             'token_name' => $tokenName,
             'balance' => 1000,
         ]);
 
-        Token::SetBalance($address, $tokenName, 1010);
+        Token::setBalance($address, $tokenName, 1010);
 
         // Act
         Token::_save();
 
         // Assert
-        $actual = $this->db->getTokenCollection()->findOne(['address' => $address, 'token_name' => $tokenName]);
+        $actual = self::$db->getTokenCollection()->findOne(['address' => $address, 'token_name' => $tokenName]);
         $this->assertSame(1010, $actual['balance']);
+    }
+
+    public function testGivenTokenDataThenLoad(): void
+    {
+        // Arrange
+        Token::_reset();
+        $address = NodeInfo::getAddress();
+        $saveData = [
+            [
+                'address' => $address,
+                'token_name' => 'Token-11',
+                'balance' => 100000,
+            ],
+            [
+                'address' => $address,
+                'token_name' => 'Token-12',
+                'balance' => 200000,
+            ],
+            [
+                'address' => '0x6fa9fd4cf2a675b0ca54fb99ce8b1d14600178053dc2f2',
+                'token_name' => 'Token-12',
+                'balance' => 4000000
+            ]
+        ];
+        self::$db->getTokenCollection()->insertMany($saveData);
+
+        Token::loadToken($address, $saveData[0]['token_name']);
+        Token::loadToken($address, $saveData[1]['token_name']);
+        Token::loadToken($saveData[2]['address'], $saveData[2]['token_name']);
+
+        // Act
+        Token::_load();
+
+        // Assert
+        $allAddress = self::$sut->getAllAddressList();
+        $allTokenName = self::$sut->getAllTokenNameList();
+        $allTokenBalance = self::$sut->getAllTokenBalance();
+
+        $this->assertIsArray($allAddress);
+        $this->assertIsArray($allTokenName);
+        $this->assertIsArray($allTokenBalance);
+
+        $this->assertContains($address, $allAddress);
+        $this->assertContains($saveData[0]['token_name'], $allTokenName);
+        $this->assertContains($saveData[0]['balance'], $allTokenBalance[$address]);
+        $this->assertArrayHasKey($saveData[2]['token_name'], $allTokenBalance[$saveData[2]['address']]);
+    }
+
+    public function testGivenNoneAddressDataThenLoad(): void
+    {
+        // Arrange
+        Token::_reset();
+        $address = NodeInfo::getAddress();
+        $saveData = [
+            [
+                'address' => $address,
+                'token_name' => 'Token-11',
+                'balance' => 100000,
+            ],
+            [
+                'address' => $address,
+                'token_name' => 'Token-12',
+                'balance' => 200000,
+            ],
+            [
+                'address' => '0x6fa9fd4cf2a675b0ca54fb99ce8b1d14600178053dc2f2',
+                'token_name' => 'Token-12',
+                'balance' => 4000000
+            ]
+        ];
+        self::$db->getTokenCollection()->insertMany($saveData);
+
+        // Act
+        Token::_load();
+
+        // Assert
+        $allAddress = self::$sut->getAllAddressList();
+        $allTokenName = self::$sut->getAllTokenNameList();
+        $allTokenBalance = self::$sut->getAllTokenBalance();
+
+        $this->assertEmpty($allAddress);
+        $this->assertEmpty($allTokenName);
+        $this->assertEmpty($allTokenBalance);
     }
 }
