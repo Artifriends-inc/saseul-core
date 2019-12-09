dfa26debfb36727527b8cc5254cf23605819caa4
Thomas Sungjin Kang <ujuc@ujuc.kr>
2019-12-09 16:48:59 +0900

    SC-226 Core\Block 에서 datas 삭제 (#194)
    - 사용하지 않는 lastBlocks 삭제
    - blockByNumber는 getBlockInfoByNumber 와 같은 작업이라 삭제
    
    issue: SC-226

--- sourcedata/SaseulDefault/Core/Block.php
+++ sourcedata/SaseulDefault/Core/Block.php
@@ -5,12 +5,10 @@ namespace Saseul\Core;
 use Exception;
 use Saseul\Constant\MongoDb;
 use Saseul\Constant\Rule;
-use Saseul\Constant\Structure;
 use Saseul\DataAccess\Models\Block as BlockModel;
 use Saseul\DataAccess\Models\Transaction as TransactionModel;
 use Saseul\System\Database;
 use Saseul\Util\DateTime;
-use Saseul\Util\Parser;
 
 class Block
 {
@@ -52,59 +50,6 @@ class Block
             's_timestamp' => $standardTimestamp,
             'timestamp' => DateTime::Microtime(),
         ];
-    }
-
-    public static function lastBlocks(int $max_count = 100): array
-    {
-        $opt = ['sort' => ['timestamp' => -1]];
-
-        return self::datas(MongoDb::NAMESPACE_BLOCK, $max_count, [], $opt);
-    }
-
-    public static function blockByNumber(int $block_number): array
-    {
-        $query = ['block_number' => $block_number];
-
-        return self::data(MongoDb::NAMESPACE_BLOCK, $query);
-    }
-
-    public static function data(string $namespace, array $query = [], array $opt = []): array
-    {
-        $block = Structure::BLOCK;
-
-        $blocks = self::datas($namespace, 1, $query, $opt);
-
-        if (isset($blocks[0])) {
-            $block = $blocks[0];
-        }
-
-        return $block;
-    }
-
-    public static function datas(string $namespace, int $max_count, array $query = [], array $opt = []): array
-    {
-        $db = Database::getInstance();
-        $rs = $db->Query($namespace, $query, $opt);
-        $datas = [];
-
-        foreach ($rs as $item) {
-            $item = Parser::objectToArray($item);
-
-            $datas[] = [
-                'block_number' => (int) $item['block_number'] ?? 0,
-                'last_blockhash' => $item['last_blockhash'] ?? '',
-                'blockhash' => $item['blockhash'] ?? '',
-                'transaction_count' => (int) $item['transaction_count'] ?? 0,
-                's_timestamp' => (int) $item['s_timestamp'] ?? 0,
-                'timestamp' => (int) $item['timestamp'] ?? 0,
-            ];
-
-            if (count($datas) >= $max_count) {
-                break;
-            }
-        }
-
-        return $datas;
     }
 
     /**
